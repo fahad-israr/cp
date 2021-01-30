90589f041853f5e0837c718cecd97959ea3f807f
fahad00cms <fahad00cms@gmail.com>
2021-01-31 03:05:52 +0530

    LCA added

---
+++ Tress/lowestComAncestorBinaryTree.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root == p || root == q) return root;
+        
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+    if(left == null && right == null) return null; //This can't be a LCA
+    if(left != null && right != null) return root;  //This is the LCA
+        
+    return left == null ? right : left;
+    }
+}
