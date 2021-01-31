8f146c111b3ddd43ae843bceb7639fe263fa1a17
fahad00cms <fahad00cms@gmail.com>
2021-01-31 14:58:30 +0530

    Tree Traversals added

---
+++ Tress/postOrderIterativeTraversal.java
@@ -0,0 +1,16 @@
+public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<Integer> result = new LinkedList<>();
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.addFirst(p.val);  // Reverse the process of preorder
+            p = p.right;             // Reverse the process of preorder
+        } else {
+            TreeNode node = stack.pop();
+            p = node.left;           // Reverse the process of preorder
+        }
+    }
+    return result;
+}
