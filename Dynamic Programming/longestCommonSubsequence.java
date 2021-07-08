7de65290d0b323ae4c362c8dadb2028bf0c2cb85
fahad-israr <fahad00cms@gmail.com>
2021-07-08 17:52:51 +0530

    Added Iterative method for longest Common Subsequence

--- Dynamic Programming/longestCommonSubsequence.java
+++ Dynamic Programming/longestCommonSubsequence.java
@@ -57,7 +57,7 @@ class LCS{
 
     }
     
-    //======= MEMOIZATION SOLUTION ===============
+    //=============== MEMOIZATION SOLUTION ===============
     static int solve(String s1,String s2,int m,int n){
       if(m==0||n==0) return 0;
       if(dp[m][n]!=-1) return dp[m][n];
@@ -66,6 +66,29 @@ class LCS{
       
       return dp[m][n] = Math.max(solve(s1,s2,m-1,n),solve(s1,s2,m,n-1));
 
+    }
+
+    // =============== Iterative Solution ===============
+
+    static int solve(String s1,String s2,int m,int n){
+         int[][]dp = new int[x+1][y+1];
+        
+        for(int i=0;i<x+1;i++)
+            for(int j=0;j<y+1;j++){
+                
+                if(i==0||j==0)
+                    dp[i][j] = 0;
+                    
+                else
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
+    
+                else{
+                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+            
+        return dp[x][y] ;
     }
     
 }
