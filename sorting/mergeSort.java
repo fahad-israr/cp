ba7361d7f858d35dcb1230be3e26d1e4af2d5428
Fahad Israr <fahad00cms@gmail.com>
2022-04-06 03:17:07 +0530

    merge sort

--- sorting/mergeSort.java
+++ sorting/mergeSort.java
@@ -1,99 +1,41 @@
-/* Java program for Merge Sort */
-class mergeSort 
+class Solution
 {
-	// Merges two subarrays of arr[].
-	// First subarray is arr[l..m]
-	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r)
-	{
-		// Find sizes of two subarrays to be merged
-		int n1 = m - l + 1;
-		int n2 = r - m;
+	 void mergeSort(int arr[], int l, int r)
+    {
+        //Function to sort an Array using merrge sort.
+        if(l >= r){
+            return ;
+        }
+        int m = l + ((r-l)/2);
+        
+        mergeSort(arr, l, m);
+        
+        mergeSort(arr, m+1, r);
+        
+        merge(arr, l, m, r);
+    }
 
-		/* Create temp arrays */
-		int L[] = new int[n1];
-		int R[] = new int[n2];
-
-		/*Copy data to temp arrays*/
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
-
-		/* Merge the temp arrays */
-
-		// Initial indexes of first and second subarrays
-		int i = 0, j = 0;
-
-		// Initial index of merged subarry array
-		int k = l;
-    // Compare and Merge
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) 
-				arr[k++] = L[i++];
-			else 
-				arr[k++] = R[j++];
-		}
-
-		/* Copy remaining elements of L[] if any */
-		while (i < n1) 
-			arr[k++] = L[i++];
-		
-
-		/* Copy remaining elements of R[] if any */
-		while (j < n2) 
-			arr[k++] = R[j++];
-	
-	}
-
-	// Main function that sorts arr[l..r] using
-	// merge()
-	void sort(int arr[], int l, int r)
-	{
-		if (l < r) {
-			// Find the middle point
-			int m =l+ (r-l)/2;
-
-			// Sort first and second halves
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
-
-			// Merge the sorted halves
-			merge(arr, l, m, r);
-		}
-	}
-
-  
-/*
-  [3 2 1 4]
-  [3 2],[1,4]
-  [3],[2],[1],[4]
-  [2,3],[1,4]
-  [1,2,3,4]
-*/
-
-	/* A utility function to print array of size n */
-	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
-
-	// Driver code
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-
-		System.out.println("Given Array");
-		printArray(arr);
-
-		mergeSort ob = new mergeSort();
-		ob.sort(arr, 0, arr.length - 1);
-
-		System.out.println("\nSorted array");
-		printArray(arr);
-	}
+    void merge(int arr[], int l, int m, int r)
+    {
+        // This function will Merge two Sorted Arrays :
+        // Array1 : l to m.
+        // Array2: m+1 to r.
+        
+         int[] temp = new int[r-l+1];// Temp Array to store sorted result
+         
+         int p1 = l, p2 = m+1, k = 0;
+         
+         while(k < temp.length){
+             if(p1 <= m && (p2 > r || arr[p1] < arr[p2])){
+                 temp[k++] = arr[p1++];
+             } else {
+                 temp[k++] = arr[p2++];
+             }
+         }
+         
+         for(int i = 0; i< temp.length; i++){
+             arr[l+i] = temp[i];
+         }
+    }
+    
 }
-/* This code is contributed by Rajat Mishra */
